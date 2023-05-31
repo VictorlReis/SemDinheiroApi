3972e9ccb1e774cad275e1c5984a2182f57300bb
Victor Reis <victorsireis@gmail.com>
2023-05-30 23:25:30 -0300

    feat: add cors policy

--- Program.cs
+++ Program.cs
@@ -36,6 +36,16 @@ builder.Services.AddScoped<ITransactionRepository, TransactionRepository>();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(corsPolicyBuilder =>
+    {
+        corsPolicyBuilder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
 // Migrate the database
